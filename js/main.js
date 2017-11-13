438216ef03086b3241e82bec32eef5507160d518
Abby Christens <achristens@gmail.com>
2017-11-13 14:11:46 -0500

    Show modal on button click

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function(){
+  var button = document.querySelector('.signin');
+  var modal = document.querySelector('.modal');
+
+  button.addEventListener('click', function(){
+    modal.style.display = 'block';
+  });
+});
