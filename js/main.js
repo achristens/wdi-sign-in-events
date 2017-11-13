ca461d1fa5ff0b2a49693d10c11c869d20ae564d
Abby Christens <achristens@gmail.com>
2017-11-13 14:27:15 -0500

    Close button on close button click

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@
 document.addEventListener('DOMContentLoaded', function(){
   var button = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
-
+  var close = document.querySelector('.close');
   button.addEventListener('click', function(){
-    modal.style.display = 'block';
+    // button.addEventListener('transitionend', function() {
+    // modal.style.transition = 'opacity 1000ms ease-in-out'
+      modal.style.display = 'block';
+    // });
+  });
+
+  close.addEventListener('click', function(){
+    modal.style.display = 'none';
   });
 });
