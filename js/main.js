228a6f9344c912fb99ad79c0494a677e70f8be0d
Abby Christens <achristens@gmail.com>
2017-11-13 14:42:40 -0500

    Add error class around empty form field

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@
 
 document.addEventListener('DOMContentLoaded', function(){
   var button = document.querySelector('.signin');
-  var modal = document.querySelector('.modal');
-  var close = document.querySelector('.close');
+  var modal  = document.querySelector('.modal');
+  var close  = document.querySelector('.close');
+  var submit = document.querySelector('.submit');
+  var inputs  = document.querySelectorAll('input');
+
   button.addEventListener('click', function(){
     // button.addEventListener('transitionend', function() {
     // modal.style.transition = 'opacity 1000ms ease-in-out'
@@ -13,5 +16,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
   close.addEventListener('click', function(){
     modal.style.display = 'none';
+  });
+
+  submit.addEventListener('click', function(){
+    inputs.forEach(function(input){
+      if (input.value === ""){
+        input.classList.add('error');
+      }
+    });
   });
 });
